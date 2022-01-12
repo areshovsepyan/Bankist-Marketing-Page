a85c4b333b7d44dd48bf53fd31637e99ca0e13df
Ares Hovsepyan <areshovsepyan@yahoo.com>
2022-01-12 16:21:21 +0400

    implemented images lazy loading, reveal sections on loading

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ const cookieMessage = document
   .createElement('div');
 const buttonScrollTo = document
   .querySelector('.btn--scroll-to');
+const allSections = document
+  .querySelectorAll('.section');
 const section1 = document
   .querySelector('#section--1');
 const tabs = document
@@ -23,7 +25,11 @@ const tabsContainer = document
   .querySelector('.operations__tab-container');
 const tabsContent = document
   .querySelectorAll('.operations__content');
-const navBar = document.querySelector('.nav');
+const navBar = document
+  .querySelector('.nav');
+const imgTargets = document
+  .querySelectorAll('img[data-src]');
+
 
 //////////////////  HEADER STYLE CHANGED  //////////////////////////////
 /// colors H1
@@ -151,24 +157,12 @@ navBar
   .addEventListener('mouseout', hoverHandler.bind(1));
 
 //////////////////////// STICKY NAV BAR IMPLEMENTATION //////////////////////
-/// THE SCROLL EVENT (BAD PRACTICE - BAD FOR OLD MOBILE DEVICES)
-// const initialCoord = section1.getBoundingClientRect();
-
-// window.addEventListener('scroll', function() {
-//   if (window.scrollY < initialCoord.top) {
-//     navBar.classList.remove('sticky');
-//   }
-//   if (window.scrollY >= initialCoord.top) {
-//     navBar.classList.add('sticky');
-//   }
-// });
-
 /// THE INTERSECTION OBSERVER API (BETTER PRACTICE)
 const navHeight = navBar.getBoundingClientRect().height;
 const stickyNav = function(entries, _) {
   const [entry] = entries;
 
-  navBar.classList.remove('sticky')
+  navBar.classList.remove('sticky');
   if (!entry.isIntersecting) {
     navBar.classList.add('sticky');
   }
@@ -180,17 +174,52 @@ const headerObserver =
     {
       root: null,
       threshold: 0,
-      rootMargin: `-${navHeight}px`,
+      rootMargin: `-${navHeight}px`
     });
 headerObserver.observe(header);
 
+//////////////////////// REVEAL SECTIONS (SCROLLING) //////////////////////
+const revealSections = function(entries, observer) {
+  const [entry] = entries;
 
+  if (!entry.isIntersecting) return;
 
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
 
+};
+const sectionObserver = new IntersectionObserver(
+  revealSections,
+  {
+    root: null,
+    threshold: 0.15
+  });
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
+//////////////////////// LAZY LOADING IMAGES //////////////////////
+const loadImg = function(entries, observer) {
+  const [entry] = entries;
 
+  if (!entry.isIntersecting) return;
 
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function() {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
 
+const imgObserver = new IntersectionObserver(loadImg,
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+  });
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 
 
